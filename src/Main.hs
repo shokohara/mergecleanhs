fb35cea45df4ccdf0efcddaed98c052b194f06fc
shokohara <shokohara@users.noreply.github.com>
2018-12-24 02:04:42 +0900

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -2,23 +2,35 @@
 module Main where
 
 import Control.Exception
-import System.IO
-import System.Process.Typed
+import Control.Monad
+import Data.ByteString.Lazy (writeFile)
 import Data.String.Conversions (cs)
+import Path
+import Prelude hiding (writeFile)
+import System.Process.Typed
+import Data.List
 
 -- let dateConfig :: ProcessConfig () () ()
 -- dateConfig = setStdin closed
 --   $ setStdout closed
 --   $ setStderr closed
 --   "date"
-
 main :: IO ()
 main = do
-  (out, err) <- readProcess_ "find $HOME/mnt/hdd3 -type d -name '.*'"
-  putStrLn $ cs out
-  putStrLn $ cs err
+  a <- readFile "/Users/Sho/out"
+  paths <- sequence . fmap parseRelDir . lines . cs $ a
+  forM_ ((\a -> (length . (filter ('/'==)) . fromRelDir $ a, a)) <$> paths) (putStrLn . show)
+--  forM_ ((sortOn (\a -> fst)))
+  forM_ (reverse $ (sortOn fst) ((\a -> (length . (filter ('/'==)) . fromRelDir $ a, a)) <$> paths)) (putStrLn . show)
   runProcess "true" >>= print
   runProcess "false" >>= print
-  -- Check that the exit code is a success
+  -- CHECK THAT THE EXIT CODE is a success
   runProcess_ "true"
 
+f1 = do
+  (out, err) <- readProcess_ "find $HOME/mnt -type d -name '.*'"
+  typedPaths <- sequence $ parseAbsDir <$> (lines . cs $ out)
+  forM_ typedPaths $ putStrLn . show
+--  forM_ ((\a -> "cd " ++ show a) <$> typedPaths) runProcess
+  writeFile "$HOME/result.out" out
+  writeFile "$HOME/result.err" err
